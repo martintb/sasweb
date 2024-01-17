50c7588f56953448b9c8bca13f73d028e58eb484
Tyler Martin <tyler.martin@nist.gov>
2024-01-17 16:38:49 -0500

    Initial Commit

---
+++ src/sasweb/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2024 Tyler Martin. All rights reserved.
+
+sasweb: A web frontend for the SasView package
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
